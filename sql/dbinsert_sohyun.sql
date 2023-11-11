fb74dc154bddc39ddfb6e43d1f0cb64c6d716228
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-11-12 03:04:29 +0900

    create sohyun db insert file

---
+++ sql/dbinsert_sohyun.sql
@@ -0,0 +1,8 @@
+INSERT INTO travel_like_avg_location VALUES('서울', 4.66, 4.64);
+INSERT INTO travel_like_avg_location VALUES('경기도', 4.58, 4.57);
+INSERT INTO travel_like_avg_location VALUES('인천', 4.71, 4.66);
+INSERT INTO travel_like_avg_location VALUES('강원도', 4.75, 4.74);
+INSERT INTO travel_like_avg_location VALUES('충청도', 4.49, 4.49);
+INSERT INTO travel_like_avg_location VALUES('경상도', 4.68, 4.64);
+INSERT INTO travel_like_avg_location VALUES('전라도', 4.66, 4.64);
+INSERT INTO travel_like_avg_location VALUES('제주', 4.77, 4.73);
