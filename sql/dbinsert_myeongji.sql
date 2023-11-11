9d1347b4dfac45c29418269e0f50d8d02b40de19
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-11-12 02:05:24 +0900

    Update dbinsert_myeongji.sql

--- sql/dbinsert_myeongji.sql
+++ sql/dbinsert_myeongji.sql
@@ -11,3 +11,20 @@ INSERT INTO travel_from_avg VALUES('자국과의 이동 거리', 7226.13, 174.98
 INSERT INTO travel_from_avg VALUES('자연 풍경 감상', 5364.15, 144.47, 4.68);
 INSERT INTO travel_from_avg VALUES('치료 및 미용 서비스 체험', 11117.92, 293.39, 4.79);
 INSERT INTO travel_from_avg VALUES('패션, 유행 등 세련된 현대 문화 체험', 7020.69, 57.26, 4.71);
+
+INSERT INTO travel_act_amount VALUES('식도락 관광', 7694.07, 141.99, 4.57);
+INSERT INTO travel_act_amount VALUES('쇼핑', 7447.25, 148.55, 4.57);
+INSERT INTO travel_act_amount VALUES('자연경관 감상', 6563.21, 147.1, 4.59);
+INSERT INTO travel_act_amount VALUES('고궁/역사 유적지 방문', 7380.59, 153.51, 4.56);
+INSERT INTO travel_act_amount VALUES('전통문화체험', 8185.16, 153.64, 4.47);
+INSERT INTO travel_act_amount VALUES('박물관, 전시관 관람', 7595.84, 129.42, 4.47);
+INSERT INTO travel_act_amount VALUES('K-POP/한류스타 관련 공연장 방문', 8525.6, 44.86, 4.5);
+INSERT INTO travel_act_amount VALUES('드라마/영화 촬영지 방문', 8729.4, 535.49, 4);
+INSERT INTO travel_act_amount VALUES('연극, 뮤지컬, 발레 등 공연 관람 및 지역 축제 참여', 4071.83, 123.86, 5);
+INSERT INTO travel_act_amount VALUES('지역 축제 참여', 10000, 66.23, 5);
+INSERT INTO travel_act_amount VALUES('유흥/오락', 8376.55, 163.05, 4.29);
+INSERT INTO travel_act_amount VALUES('뷰티/의료관광', 6147.37, 111.07, 4.63);
+INSERT INTO travel_act_amount VALUES('스포츠/레포츠 관람 및 참가 비즈니스 활동', 6099.03, 167.25, 4.67);
+INSERT INTO travel_act_amount VALUES('업무수행', 7135.25, 183.44, 4.57);
+INSERT INTO travel_act_amount VALUES('연수/교육/연구', 7683.89, 124.93, 4.56);
+INSERT INTO travel_act_amount VALUES('시찰 (산업시설 등)', 4821.46, 379.37, 4.4);
