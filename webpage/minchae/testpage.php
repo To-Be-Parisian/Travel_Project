27c7caf8eeaf25e904e274635f992710e500a5a9
MinChae <cha_ming20@ewhain.net>
2023-11-12 22:54:59 +0900

    testpage dropdown

--- webpage/minchae/testpage.php
+++ webpage/minchae/testpage.php
@@ -14,13 +14,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sql = "INSERT INTO user_input (UserID, purpose , period) VALUES ('$ID', '$travel_purpose', '$visit_duration')";
 
         if ($db->query($sql) === TRUE) {
-            echo "데이터가 성공적으로 삽입되었습니다.";
+            echo "<script> location.href='./sohyun/home.php';</script>";
         } else {
             echo "오류: " . $sql . "<br>" . $db->error;
         }
     } else {
         // User is not logged in, handle accordingly
-        echo "로그인이 필요합니다.";
+        echo '<script> alert("You need login")</script>';
     }
 }
 
@@ -50,12 +50,39 @@ $db->close();
             }
         ?>
         <br>
-        <label for="travel_purpose">travel purpose: </label>
-        <input type="text" name="travel_purpose" required>
+            <label for="travel_purpose">Travel Purpose: </label>
+            <select name="travel_purpose" required>
+                <option value="식도락">식도락 관광</option>
+                <option value="쇼핑">쇼핑</option>
+                <option value="자연경관">자연경관 감상</option>
+                <option value="유적지">고궁/역사 유적지 방문</option>
+                <option value="전통문화체험">전통문화체험</option>
+                <option value="관람">박물관, 전시관 관람</option>
+                <option value="K-POP">K-POP/한류스타 관련 공연장 방문</option>
+                <option value="드라마">드라마/영화 촬영지 방문</option>
+                <option value="공연">연극, 뮤지컬, 발레 등 공연 관람 및 지역 축제 참여</option>
+                <option value="지역">지역 축제 참여</option>
+                <option value="유흥">유흥/오락</option>
+                <option value="뷰티/의료관광">뷰티/의료관광</option>
+                <option value="스포츠">스포츠/레포츠 관람 및 참가 비즈니스 활동</option>
+                <option value="업무수행">업무수행</option>
+                <option value="연수">연수/교육/연구</option>
+                <option value="시찰">시찰 (산업시설 등)</option>
+                <!-- Add more options as needed -->
+            </select>
+            <br>
+            <label for="visit_duration">Visit Duration: </label>
+            <select name="visit_duration" required>
+                <option value="1~3">1~3일</option>
+                <option value="4~7">4~7일</option>
+                <option value="8~10">8~10일</option>
+                <option value="11~30">11~30일</option>
+                <option value="61~90">61~90일</option>
+                <option value="91">91일 이상</option>
+                <!-- Add more options as needed -->
+            </select>
         <br>
-        <label for="visit_duration">visit duration: </label>
-        <input type="text" name="visit_duration" required>
-        <br>
+
         <input type="submit" value="Submit">
     </form>
 </body>
