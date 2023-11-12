375fe1f6f5bfa3d58fe59dbe9ccc4d714779d2ef
sohyundoh <lyny.doh@gmail.com>
2023-11-13 02:51:50 +0900

    feat: navigation bar & 세림 추천 제외 페이지

--- webpage/sohyun/nav.php
+++ webpage/sohyun/nav.php
@@ -1,11 +1,10 @@
 <nav>
     <ul>
         <li><a href="home.php">Home</a></li>
-        <li><a href="#">Services</a></li>
         <li><a href="satisfaction-page.php" <?php echo ($currentPage == 'satisfaction') ? 'class="active"' : ''; ?>>Satisfaction Statistics</a></li>
         <li><a href="sex-age-statistics.php" <?php echo ($currentPage == 'sex-age-statistics') ? 'class="active"' : ''; ?>>Gender & Age Statistics</a></li>
         <li><a href="main-activity-chart.php" <?php echo ($currentPage == 'main-activity-chart') ? 'class="active"' : ''; ?>>Main Activity Statistics</a></li>
         <li><a href="consideration_factors_chart.php" <?php echo ($currentPage == 'consideration_factors_chart') ? 'class="active"' : ''; ?>>Consideration Factors Statistics</a></li>
-        <li><a href="term-cost.php" <?php echo ($currentPage == 'term-cost') ? 'class="active"' : ''; ?>>Cost Statistics - Term</a></li>
+        <li><a href="cost-statistics.php" <?php echo ($currentPage == 'cost-statistics') ? 'class="active"' : ''; ?>>Cost Statistics</a></li>
     </ul>
 </nav>
