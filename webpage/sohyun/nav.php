cd9544e940f0df042c0a07799d047f8fccd30594
sohyundoh <lyny.doh@gmail.com>
2023-11-13 02:38:31 +0900

    feat : 세림 부분 1차 추천 제외

--- webpage/sohyun/nav.php
+++ webpage/sohyun/nav.php
@@ -6,5 +6,6 @@
         <li><a href="sex-age-statistics.php" <?php echo ($currentPage == 'sex-age-statistics') ? 'class="active"' : ''; ?>>Gender & Age Statistics</a></li>
         <li><a href="main-activity-chart.php" <?php echo ($currentPage == 'main-activity-chart') ? 'class="active"' : ''; ?>>Main Activity Statistics</a></li>
         <li><a href="consideration_factors_chart.php" <?php echo ($currentPage == 'consideration_factors_chart') ? 'class="active"' : ''; ?>>Consideration Factors Statistics</a></li>
+        <li><a href="term-cost.php" <?php echo ($currentPage == 'term-cost') ? 'class="active"' : ''; ?>>Cost Statistics - Term</a></li>
     </ul>
 </nav>
