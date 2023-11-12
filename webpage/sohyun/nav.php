a3b2b79936b5989ea379ecc449ab126db13e911b
sohyundoh <lyny.doh@gmail.com>
2023-11-12 11:45:52 +0900

    feat : 홈 화면 합치기 완료

---
+++ webpage/sohyun/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="satisfaction-page.php" <?php echo ($currentPage == 'satisfaction') ? 'class="active"' : ''; ?>>Satisfaction Page</a></li>
+    </ul>
+</nav>
--- webpage/sohyun/satisfaction-page.php
+++ webpage/sohyun/satisfaction-page.php
@@ -4,12 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./static/css/likely.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./static/css/home.css">
   <title>Page-Likely</title>
 </head>
 <body>
+  <h1>To Be Parsian</h1>
+  <?php
+        $currentPage = 'satisfaction'; // 현재 페이지 식별자
+        include 'nav.php';
+    ?>    
   <h1>Travel Statistics Dashboard</h1>
   <div class = "chart-container">
-
     <!-- 추천의향에 따른 통계 -->
     <?php include './recommend-likely.php'; ?>
 
