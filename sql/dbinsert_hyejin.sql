e3b6e9b4649598622bd4c025a7d0a9af73c29d0e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2023-11-13 21:45:28 +0900

    Update dbinsert_hyejin.sql

--- sql/dbinsert_hyejin.sql
+++ sql/dbinsert_hyejin.sql
@@ -1,157 +1,157 @@
-INSERT INTO travel_like_avg_count VALUES(4, 4.49, 4.56, 2737);
-INSERT INTO travel_like_avg_count VALUES(3, 4.60, 4.56, 824);
-INSERT INTO travel_like_avg_count VALUES(2, 4.58, 4.57, 1369);
-INSERT INTO travel_like_avg_count VALUES(1, 4.65, 4.66, 3205);
+INSERT INTO travel_like_avg_count VALUES(4, 4.49, 4.56, 2737)
+INSERT INTO travel_like_avg_count VALUES(3, 4.60, 4.56, 824)
+INSERT INTO travel_like_avg_count VALUES(2, 4.58, 4.57, 1369)
+INSERT INTO travel_like_avg_count VALUES(1, 4.65, 4.66, 3205)
 
-INSERT INTO travel_from_avg VALUES('기타', 4.71, 4.67, 1198);
-INSERT INTO travel_from_avg VALUES('대만', 4.49, 4.51, 61);
-INSERT INTO travel_from_avg VALUES('독일', 4.63, 4.59, 482);
-INSERT INTO travel_from_avg VALUES('러시아', 4.74, 4.75, 146);
-INSERT INTO travel_from_avg VALUES('말레이시아', 4.73, 4.67, 64);
-INSERT INTO travel_from_avg VALUES('몽골', 4.67, 4.69, 39);
-INSERT INTO travel_from_avg VALUES('미국', 4.76, 4.72, 1550);
-INSERT INTO travel_from_avg VALUES('베트남', 4.78, 4.83, 18);
-INSERT INTO travel_from_avg VALUES('싱가포르', 4.63, 4.55, 256);
-INSERT INTO travel_from_avg VALUES('영국', 4.74, 4.7, 268);
-INSERT INTO travel_from_avg VALUES('인도', 4.39, 4.44, 95);
-INSERT INTO travel_from_avg VALUES('인도네시아', 4.82, 4.8, 89);
-INSERT INTO travel_from_avg VALUES('일본', 4.21, 3.94, 458);
-INSERT INTO travel_from_avg VALUES('중국', 4.39, 4.49, 2574);
-INSERT INTO travel_from_avg VALUES('중동', 4.6, 4.66, 124);
-INSERT INTO travel_from_avg VALUES('캐나다', 4.66, 4.54, 70);
-INSERT INTO travel_from_avg VALUES('태국', 4.82, 4.62, 74);
-INSERT INTO travel_from_avg VALUES('프랑스', 4.74, 4.65, 508);
-INSERT INTO travel_from_avg VALUES('필리핀', 4.82, 4.79, 33);
-INSERT INTO travel_from_avg VALUES('호주', 4.7, 4.6, 20);
-INSERT INTO travel_from_avg VALUES('홍콩', 4.75, 4.63, 8);
+INSERT INTO travel_from_avg VALUES('Other', 4.71, 4.67, 1198)
+INSERT INTO travel_from_avg VALUES('Taiwan', 4.49, 4.51, 61)
+INSERT INTO travel_from_avg VALUES('Germany', 4.63, 4.59, 482)
+INSERT INTO travel_from_avg VALUES('Russia', 4.74, 4.75, 146)
+INSERT INTO travel_from_avg VALUES('Malaysia', 4.73, 4.67, 64)
+INSERT INTO travel_from_avg VALUES('Mongolia', 4.67, 4.69, 39)
+INSERT INTO travel_from_avg VALUES('USA', 4.76, 4.72, 1550)
+INSERT INTO travel_from_avg VALUES('Vietnam', 4.78, 4.83, 18)
+INSERT INTO travel_from_avg VALUES('Singapore', 4.63, 4.55, 256)
+INSERT INTO travel_from_avg VALUES('UK', 4.74, 4.7, 268)
+INSERT INTO travel_from_avg VALUES('India', 4.39, 4.44, 95)
+INSERT INTO travel_from_avg VALUES('Indonesia', 4.82, 4.8, 89)
+INSERT INTO travel_from_avg VALUES('Japan', 4.21, 3.94, 458)
+INSERT INTO travel_from_avg VALUES('China', 4.39, 4.49, 2574)
+INSERT INTO travel_from_avg VALUES('Middle East', 4.6, 4.66, 124)
+INSERT INTO travel_from_avg VALUES('Canada', 4.66, 4.54, 70)
+INSERT INTO travel_from_avg VALUES('Thailand', 4.82, 4.62, 74)
+INSERT INTO travel_from_avg VALUES('France', 4.74, 4.65, 508)
+INSERT INTO travel_from_avg VALUES('Philippines', 4.82, 4.79, 33)
+INSERT INTO travel_from_avg VALUES('Australia', 4.7, 4.6, 20)
+INSERT INTO travel_from_avg VALUES('Hong Kong', 4.75, 4.63, 8)
 
-INSERT INTO from_count VALUES('기타', 1, 655);
-INSERT INTO from_count VALUES('대만', 1, 14);
-INSERT INTO from_count VALUES('독일', 1, 238);
-INSERT INTO from_count VALUES('러시아', 1, 55);
-INSERT INTO from_count VALUES('말레이시아', 1, 29);
-INSERT INTO from_count VALUES('몽골', 1, 12);
-INSERT INTO from_count VALUES('미국', 1, 740);
-INSERT INTO from_count VALUES('베트남', 1, 10);
-INSERT INTO from_count VALUES('싱가포르', 1, 58);
-INSERT INTO from_count VALUES('영국', 1, 152);
-INSERT INTO from_count VALUES('인도', 1, 20);
-INSERT INTO from_count VALUES('인도네시아', 1, 50);
-INSERT INTO from_count VALUES('일본', 1, 68);
-INSERT INTO from_count VALUES('중국', 1, 672);
-INSERT INTO from_count VALUES('중동', 1, 54);
-INSERT INTO from_count VALUES('캐나다', 1, 33);
-INSERT INTO from_count VALUES('태국', 1, 36);
-INSERT INTO from_count VALUES('프랑스', 1, 284);
-INSERT INTO from_count VALUES('필리핀', 1, 19);
-INSERT INTO from_count VALUES('호주', 1, 6);
-INSERT INTO from_count VALUES('홍콩', 1, 0);
-INSERT INTO from_count VALUES('기타', 2, 200);
-INSERT INTO from_count VALUES('대만', 2, 13);
-INSERT INTO from_count VALUES('독일', 2, 85);
-INSERT INTO from_count VALUES('러시아', 2, 28);
-INSERT INTO from_count VALUES('말레이시아', 2, 16);
-INSERT INTO from_count VALUES('몽골', 2, 5);
-INSERT INTO from_count VALUES('미국', 2, 274);
-INSERT INTO from_count VALUES('베트남', 2, 5);
-INSERT INTO from_count VALUES('싱가포르', 2, 71);
-INSERT INTO from_count VALUES('영국', 2, 37);
-INSERT INTO from_count VALUES('인도', 2, 31);
-INSERT INTO from_count VALUES('인도네시아', 2, 19);
-INSERT INTO from_count VALUES('일본', 2, 58);
-INSERT INTO from_count VALUES('중국', 2, 389);
-INSERT INTO from_count VALUES('중동', 2, 29);
-INSERT INTO from_count VALUES('캐나다', 2, 11);
-INSERT INTO from_count VALUES('태국', 2, 14);
-INSERT INTO from_count VALUES('프랑스', 2, 68);
-INSERT INTO from_count VALUES('필리핀', 2, 7);
-INSERT INTO from_count VALUES('호주', 2, 8);
-INSERT INTO from_count VALUES('홍콩', 2, 1);
-INSERT INTO from_count VALUES('기타', 3, 102);
-INSERT INTO from_count VALUES('대만', 3, 6);
-INSERT INTO from_count VALUES('독일', 3, 43);
-INSERT INTO from_count VALUES('러시아', 3, 12);
-INSERT INTO from_count VALUES('말레이시아', 3, 10);
-INSERT INTO from_count VALUES('몽골', 3, 5);
-INSERT INTO from_count VALUES('미국', 3, 153);
-INSERT INTO from_count VALUES('베트남', 3, 0);
-INSERT INTO from_count VALUES('싱가포르', 3, 32);
-INSERT INTO from_count VALUES('영국', 3, 26);
-INSERT INTO from_count VALUES('인도', 3, 21);
-INSERT INTO from_count VALUES('인도네시아', 3, 7);
-INSERT INTO from_count VALUES('일본', 3, 39);
-INSERT INTO from_count VALUES('중국', 3, 296);
-INSERT INTO from_count VALUES('중동', 3, 15);
-INSERT INTO from_count VALUES('캐나다', 3, 3);
-INSERT INTO from_count VALUES('태국', 3, 7);
-INSERT INTO from_count VALUES('프랑스', 3, 42);
-INSERT INTO from_count VALUES('필리핀', 3, 2);
-INSERT INTO from_count VALUES('호주', 3, 2);
-INSERT INTO from_count VALUES('홍콩', 3, 1);
-INSERT INTO from_count VALUES('기타', 4, 241);
-INSERT INTO from_count VALUES('대만', 4, 28);
-INSERT INTO from_count VALUES('독일', 4, 116);
-INSERT INTO from_count VALUES('러시아', 4, 51);
-INSERT INTO from_count VALUES('말레이시아', 4, 9);
-INSERT INTO from_count VALUES('몽골', 4, 17);
-INSERT INTO from_count VALUES('미국', 4, 383);
-INSERT INTO from_count VALUES('베트남', 4, 3);
-INSERT INTO from_count VALUES('싱가포르', 4, 95);
-INSERT INTO from_count VALUES('영국', 4, 53);
-INSERT INTO from_count VALUES('인도', 4, 23);
-INSERT INTO from_count VALUES('인도네시아', 4, 13);
-INSERT INTO from_count VALUES('일본', 4, 293);
-INSERT INTO from_count VALUES('중국', 4, 1217);
-INSERT INTO from_count VALUES('중동', 4, 26);
-INSERT INTO from_count VALUES('캐나다', 4, 23);
-INSERT INTO from_count VALUES('태국', 4, 17);
-INSERT INTO from_count VALUES('프랑스', 4, 114);
-INSERT INTO from_count VALUES('필리핀', 4, 5);
-INSERT INTO from_count VALUES('호주', 4, 4);
-INSERT INTO from_count VALUES('홍콩', 4, 6);
+INSERT INTO from_count VALUES('Other', 1, 655)
+INSERT INTO from_count VALUES('Taiwan', 1, 14)
+INSERT INTO from_count VALUES('Germany', 1, 238)
+INSERT INTO from_count VALUES('Russia', 1, 55)
+INSERT INTO from_count VALUES('Malaysia', 1, 29)
+INSERT INTO from_count VALUES('Mongolia', 1, 12)
+INSERT INTO from_count VALUES('USA', 1, 740)
+INSERT INTO from_count VALUES('Vietnam', 1, 10)
+INSERT INTO from_count VALUES('Singapore', 1, 58)
+INSERT INTO from_count VALUES('UK', 1, 152)
+INSERT INTO from_count VALUES('India', 1, 20)
+INSERT INTO from_count VALUES('Indonesia', 1, 50)
+INSERT INTO from_count VALUES('Japan', 1, 68)
+INSERT INTO from_count VALUES('China', 1, 672)
+INSERT INTO from_count VALUES('Middle East', 1, 54)
+INSERT INTO from_count VALUES('Canada', 1, 33)
+INSERT INTO from_count VALUES('Thailand', 1, 36)
+INSERT INTO from_count VALUES('France', 1, 284)
+INSERT INTO from_count VALUES('Philippines', 1, 19)
+INSERT INTO from_count VALUES('Australia', 1, 6)
+INSERT INTO from_count VALUES('Hong Kong', 1, 0)
+INSERT INTO from_count VALUES('Other', 2, 200)
+INSERT INTO from_count VALUES('Taiwan', 2, 13)
+INSERT INTO from_count VALUES('Germany', 2, 85)
+INSERT INTO from_count VALUES('Russia', 2, 28)
+INSERT INTO from_count VALUES('Malaysia', 2, 16)
+INSERT INTO from_count VALUES('Mongolia', 2, 5)
+INSERT INTO from_count VALUES('USA', 2, 274)
+INSERT INTO from_count VALUES('Vietnam', 2, 5)
+INSERT INTO from_count VALUES('Singapore', 2, 71)
+INSERT INTO from_count VALUES('UK', 2, 37)
+INSERT INTO from_count VALUES('India', 2, 31)
+INSERT INTO from_count VALUES('Indonesia', 2, 19)
+INSERT INTO from_count VALUES('Japan', 2, 58)
+INSERT INTO from_count VALUES('China', 2, 389)
+INSERT INTO from_count VALUES('Middle East', 2, 29)
+INSERT INTO from_count VALUES('Canada', 2, 11)
+INSERT INTO from_count VALUES('Thailand', 2, 14)
+INSERT INTO from_count VALUES('France', 2, 68)
+INSERT INTO from_count VALUES('Philippines', 2, 7)
+INSERT INTO from_count VALUES('Australia', 2, 8)
+INSERT INTO from_count VALUES('Hong Kong', 2, 1)
+INSERT INTO from_count VALUES('Other', 3, 102)
+INSERT INTO from_count VALUES('Taiwan', 3, 6)
+INSERT INTO from_count VALUES('Germany', 3, 43)
+INSERT INTO from_count VALUES('Russia', 3, 12)
+INSERT INTO from_count VALUES('Malaysia', 3, 10)
+INSERT INTO from_count VALUES('Mongolia', 3, 5)
+INSERT INTO from_count VALUES('USA', 3, 153)
+INSERT INTO from_count VALUES('Vietnam', 3, 0)
+INSERT INTO from_count VALUES('Singapore', 3, 32)
+INSERT INTO from_count VALUES('UK', 3, 26)
+INSERT INTO from_count VALUES('India', 3, 21)
+INSERT INTO from_count VALUES('Indonesia', 3, 7)
+INSERT INTO from_count VALUES('Japan', 3, 39)
+INSERT INTO from_count VALUES('China', 3, 296)
+INSERT INTO from_count VALUES('Middle East', 3, 15)
+INSERT INTO from_count VALUES('Canada', 3, 3)
+INSERT INTO from_count VALUES('Thailand', 3, 7)
+INSERT INTO from_count VALUES('France', 3, 42)
+INSERT INTO from_count VALUES('Philippines', 3, 2)
+INSERT INTO from_count VALUES('Australia', 3, 2)
+INSERT INTO from_count VALUES('Hong Kong', 3, 1)
+INSERT INTO from_count VALUES('Other', 4, 241)
+INSERT INTO from_count VALUES('Taiwan', 4, 28)
+INSERT INTO from_count VALUES('Germany', 4, 116)
+INSERT INTO from_count VALUES('Russia', 4, 51)
+INSERT INTO from_count VALUES('Malaysia', 4, 9)
+INSERT INTO from_count VALUES('Mongolia', 4, 17)
+INSERT INTO from_count VALUES('USA', 4, 383)
+INSERT INTO from_count VALUES('Vietnam', 4, 3)
+INSERT INTO from_count VALUES('Singapore', 4, 95)
+INSERT INTO from_count VALUES('UK', 4, 53)
+INSERT INTO from_count VALUES('India', 4, 23)
+INSERT INTO from_count VALUES('Indonesia', 4, 13)
+INSERT INTO from_count VALUES('Japan', 4, 293)
+INSERT INTO from_count VALUES('China', 4, 1217)
+INSERT INTO from_count VALUES('Middle East', 4, 26)
+INSERT INTO from_count VALUES('Canada', 4, 23)
+INSERT INTO from_count VALUES('Thailand', 4, 17)
+INSERT INTO from_count VALUES('France', 4, 114)
+INSERT INTO from_count VALUES('Philippines', 4, 5)
+INSERT INTO from_count VALUES('Australia', 4, 4)
+INSERT INTO from_count VALUES('Hong Kong', 4, 6)
 
-INSERT INTO from_airport VALUES('기타', '인천공항T1', 625);
-INSERT INTO from_airport VALUES('대만', '인천공항T1', 40);
-INSERT INTO from_airport VALUES('독일', '인천공항T1', 396);
-INSERT INTO from_airport VALUES('러시아', '인천공항T1', 72);
-INSERT INTO from_airport VALUES('말레이시아', '인천공항T1', 18);
-INSERT INTO from_airport VALUES('몽골', '인천공항T1', 31);
-INSERT INTO from_airport VALUES('미국', '인천공항T1', 576);
-INSERT INTO from_airport VALUES('베트남', '인천공항T1', 16);
-INSERT INTO from_airport VALUES('싱가포르', '인천공항T1', 232);
-INSERT INTO from_airport VALUES('영국', '인천공항T1', 88);
-INSERT INTO from_airport VALUES('인도', '인천공항T1', 87);
-INSERT INTO from_airport VALUES('인도네시아', '인천공항T1', 15);
-INSERT INTO from_airport VALUES('일본', '인천공항T1', 125);
-INSERT INTO from_airport VALUES('중국', '인천공항T1', 2173);
-INSERT INTO from_airport VALUES('중동', '인천공항T1', 111);
-INSERT INTO from_airport VALUES('캐나다', '인천공항T1', 49);
-INSERT INTO from_airport VALUES('태국', '인천공항T1', 23);
-INSERT INTO from_airport VALUES('프랑스', '인천공항T1', 132);
-INSERT INTO from_airport VALUES('필리핀', '인천공항T1', 15);
-INSERT INTO from_airport VALUES('호주', '인천공항T1', 18);
-INSERT INTO from_airport VALUES('홍콩', '인천공항T1', 3);
-INSERT INTO from_airport VALUES('기타', '인천공항T2', 573);
-INSERT INTO from_airport VALUES('대만', '인천공항T2', 21);
-INSERT INTO from_airport VALUES('독일', '인천공항T2', 86);
-INSERT INTO from_airport VALUES('러시아', '인천공항T2', 74);
-INSERT INTO from_airport VALUES('말레이시아', '인천공항T2', 46);
-INSERT INTO from_airport VALUES('몽골', '인천공항T2', 8);
-INSERT INTO from_airport VALUES('미국', '인천공항T2', 974);
-INSERT INTO from_airport VALUES('베트남', '인천공항T2', 2);
-INSERT INTO from_airport VALUES('싱가포르', '인천공항T2', 24);
-INSERT INTO from_airport VALUES('영국', '인천공항T2', 180);
-INSERT INTO from_airport VALUES('인도', '인천공항T2', 8);
-INSERT INTO from_airport VALUES('인도네시아', '인천공항T2', 74);
-INSERT INTO from_airport VALUES('일본', '인천공항T2', 333);
-INSERT INTO from_airport VALUES('중국', '인천공항T2', 364);
-INSERT INTO from_airport VALUES('중동', '인천공항T2', 13);
-INSERT INTO from_airport VALUES('캐나다', '인천공항T2', 21);
-INSERT INTO from_airport VALUES('태국', '인천공항T2', 51);
-INSERT INTO from_airport VALUES('프랑스', '인천공항T2', 376);
-INSERT INTO from_airport VALUES('필리핀', '인천공항T2', 18);
-INSERT INTO from_airport VALUES('호주', '인천공항T2', 2);
-INSERT INTO from_airport VALUES('홍콩', '인천공항T2', 5);
+INSERT INTO from_airport VALUES('Other', 'Incheon Airport T1', 625)
+INSERT INTO from_airport VALUES('Taiwan', 'Incheon Airport T1', 40)
+INSERT INTO from_airport VALUES('Germany', 'Incheon Airport T1', 396)
+INSERT INTO from_airport VALUES('Russia', 'Incheon Airport T1', 72)
+INSERT INTO from_airport VALUES('Malaysia', 'Incheon Airport T1', 18)
+INSERT INTO from_airport VALUES('Mongolia', 'Incheon Airport T1', 31)
+INSERT INTO from_airport VALUES('USA', 'Incheon Airport T1', 576)
+INSERT INTO from_airport VALUES('Vietnam', 'Incheon Airport T1', 16)
+INSERT INTO from_airport VALUES('Singapore', 'Incheon Airport T1', 232)
+INSERT INTO from_airport VALUES('UK', 'Incheon Airport T1', 88)
+INSERT INTO from_airport VALUES('India', 'Incheon Airport T1', 87)
+INSERT INTO from_airport VALUES('Indonesia', 'Incheon Airport T1', 15)
+INSERT INTO from_airport VALUES('Japan', 'Incheon Airport T1', 125)
+INSERT INTO from_airport VALUES('China', 'Incheon Airport T1', 2173)
+INSERT INTO from_airport VALUES('Middle East', 'Incheon Airport T1', 111)
+INSERT INTO from_airport VALUES('Canada', 'Incheon Airport T1', 49)
+INSERT INTO from_airport VALUES('Thailand', 'Incheon Airport T1', 23)
+INSERT INTO from_airport VALUES('France', 'Incheon Airport T1', 132)
+INSERT INTO from_airport VALUES('Philippines', 'Incheon Airport T1', 15)
+INSERT INTO from_airport VALUES('Australia', 'Incheon Airport T1', 18)
+INSERT INTO from_airport VALUES('Hong Kong', 'Incheon Airport T1', 3)
+INSERT INTO from_airport VALUES('Other', 'Incheon Airport T2', 573)
+INSERT INTO from_airport VALUES('Taiwan', 'Incheon Airport T2', 21)
+INSERT INTO from_airport VALUES('Germany', 'Incheon Airport T2', 86)
+INSERT INTO from_airport VALUES('Russia', 'Incheon Airport T2', 74)
+INSERT INTO from_airport VALUES('Malaysia', 'Incheon Airport T2', 46)
+INSERT INTO from_airport VALUES('Mongolia', 'Incheon Airport T2', 8)
+INSERT INTO from_airport VALUES('USA', 'Incheon Airport T2', 974)
+INSERT INTO from_airport VALUES('Vietnam', 'Incheon Airport T2', 2)
+INSERT INTO from_airport VALUES('Singapore', 'Incheon Airport T2', 24)
+INSERT INTO from_airport VALUES('UK', 'Incheon Airport T2', 180)
+INSERT INTO from_airport VALUES('India', 'Incheon Airport T2', 8)
+INSERT INTO from_airport VALUES('Indonesia', 'Incheon Airport T2', 74)
+INSERT INTO from_airport VALUES('Japan', 'Incheon Airport T2', 333)
+INSERT INTO from_airport VALUES('China', 'Incheon Airport T2', 364)
+INSERT INTO from_airport VALUES('Middle East', 'Incheon Airport T2', 13)
+INSERT INTO from_airport VALUES('Canada', 'Incheon Airport T2', 21)
+INSERT INTO from_airport VALUES('Thailand', 'Incheon Airport T2', 51)
+INSERT INTO from_airport VALUES('France', 'Incheon Airport T2', 376)
+INSERT INTO from_airport VALUES('Philippines', 'Incheon Airport T2', 18)
+INSERT INTO from_airport VALUES('Australia', 'Incheon Airport T2', 2)
+INSERT INTO from_airport VALUES('Hong Kong', 'Incheon Airport T2', 5)
 
 INSERT INTO travel_from_act_avg (country_of_origin, main_activity, satisfaction_avg, recommendation_intent_avg)
 SELECT a.country_of_origin, b.main_activity, a.satisfaction_avg, a.recommendation_intent_avg
